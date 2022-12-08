25d58f684f395851c950e2aacb9a8bb19f2c0d94
Vamik76 <merts_v@mail.ru>
2022-12-09 05:58:37 +1000

    HW4C#_task2end

---
+++ task2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+Console.WriteLine ("Введите чиcло");
+string a = Console.ReadLine ();
+int i=0,z=0;
+for (i=0;i<a.Length;i++)
+{
+    if (Char.IsDigit(a[i])==false)
+    {
+        Console.WriteLine("Введено не чиcло");
+        Environment.Exit(0);
+    }
+    else
+        z=z+Convert.ToInt32(a[i]-48);
+        
+    
+}
+Console.WriteLine(z);
